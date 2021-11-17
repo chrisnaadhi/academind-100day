f75a961205278f016c576e235170d94026a907cb
chrisnaadhi <chrisnaadhip@gmail.com>
2021-11-17 22:15:26 +0700

    dynamic & static express

--- 7th-week/express-full/app.js
+++ 7th-week/express-full/app.js
@@ -1,46 +1,54 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/restaurants', (req, res) => {
-  res.render('restaurants');
+app.get("/restaurants", (req, res) => {
+  const filePath = path.join(__dirname, "data", "restaurant.json");
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurant = JSON.parse(fileData);
+  res.render("restaurants", {
+    numberOfRestaurants: storedRestaurant.length,
+    restaurants: storedRestaurant,
+  });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about');
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
-app.get('/recommend', (req, res) => {
-  res.render('recommend');
+app.get("/recommend", (req, res) => {
+  res.render("recommend");
 });
 
-app.post('/recommend', (req, res) => {
+app.post("/recommend", (req, res) => {
   const restaurant = req.body;
-  const filePath = path.join(__dirname, 'data', 'restaurant.json');
+  const filePath = path.join(__dirname, "data", "restaurant.json");
   const fileData = fs.readFileSync(filePath);
   const storedRestaurant = JSON.parse(fileData);
 
   storedRestaurant.push(restaurant);
   fs.writeFileSync(filePath, JSON.stringify(storedRestaurant));
 
-  res.redirect('/confirm');
+  res.redirect("/confirm");
 });
 
-app.get('/confirm', (req, res) => {
-  res.render('confirm');
+app.get("/confirm", (req, res) => {
+  res.render("confirm");
 });
-
-
 
 app.listen(3000);
