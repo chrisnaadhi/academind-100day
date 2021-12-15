21ffe093d177a82640742a38740a1d2075888aad
Chrisna Adhi Pranoto <chrisnaadhip@gmail.com>
2021-12-15 17:16:35 +0700

    10th week

---
+++ 10th-week/data/database.js
@@ -0,0 +1,22 @@
+const mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+
+let database;
+
+const connect = async () => {
+  const client = await MongoClient.connect("mongodb://localhost:27017");
+  database = client.db("blog");
+};
+
+const getDb = () => {
+  if (!database) {
+    throw { message: "Database connection not Estabilished!" };
+  }
+  return database;
+};
+
+module.exports = {
+  connectToDatabase: connect,
+  getDb: getDb,
+};
